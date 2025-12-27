ab6061b37342a2bc14b75aa38bec30a8ad1fa2aa
stanvr130 <stanleyokafor08@gmail.com>
2025-12-27 03:23:04 -0800

    Update app config and dependencies

--- src/app.js
+++ src/app.js
@@ -2,6 +2,39 @@ import express from "express";
 import { protect } from "./middleware/auth.js";
 import adminRoutes from "./routes/admin/route.js";
 import authRoutes from "./routes/auth/route.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LMS Backend API",
+      version: "1.0.0",
+      description: "API documentation for the Learning Management System",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+        description: "Development server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+
+  apis: ["./src/routes/**/*.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
 
 const createApp = () => {
   const app = express();
@@ -10,6 +43,9 @@ const createApp = () => {
   app.get("/", (_req, res) => {
     res.send("Hello world");
   });
+
+   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
   app.use("/api/auth", authRoutes);
 
